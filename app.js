934d8ece6ce250af470ef97c8603fe0f995c70fd
trznt <trznt.technologies@gmail.com>
2018-12-04 20:54:00 +0530

    set https

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var ws = require('./lib/webserver.js');
-var app = ws.init();
+var app2 = ws.init();
 var server;
+var secure_server;
 
 var dbconn = require('./lib/dbconn.js');
 var dbhandle;
 var db;
+
+var https = require('https');
+var app = ws.init();
+var certoptions;
 
 dbconn.openDB(0,function (error, res) {
   if (error)
@@ -13,9 +18,15 @@ dbconn.openDB(0,function (error, res) {
     console.log("DBCONN is now set");
     dbhandle = res; // Setting DB Handle.
     db = dbhandle.db("optin"); //Setting db to point to the instance we are looking to write into.
-    server = ws.listen(app);
+    server = ws.listen(app2);
     console.log("Server is listening at http://%s:%s", server.address().address, server.address().port);
+    secure_server = ws.listens(app);
   }
+});
+
+app2.get('/',function(req,res){
+  res.sendFile(__dirname + '/html/index.html')
+  console.log('accessed Index');
 });
 
 app.get('/',function(req,res){
